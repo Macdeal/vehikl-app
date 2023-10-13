adfc55ec4f593fa0dbb12518236e32f938bd9713
Akhil P Jose <akhil.jose@vonnue.com>
2023-10-13 12:29:34 +0530

    fix: header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,16 @@
-import RightArrow from '../../assets/header/rightArrow.svg?react';
 import Logo from '../../assets/header/logo.svg?react';
+import RightArrow from '../../assets/header/rightArrow.svg?react';
 
 const Header = () => (
-
 	<header className="bg-gray-900">
 		<div className="layout mx-auto px-4 max-w-6xl">
-			<nav className="flex items-center h-20 font-semibold text-white uppercase">
-				<Logo className="h-10" />
+			<nav className="flex items-center h-20 font-semibold text-white">
+				<Logo className="h-10 mr-auto" />
 				<div className="flex">
-					<a href="/">home</a>
-					<a href="/">how we work</a>
-					<a href="/">team</a>
-					<a href="/">we&apos;re hiring!</a>
+					<a className="after:transition-all text-gery-text uppercase p-2 tracking-wider text-sm transition-all relative mr-2 last-of-type:mr-0 hover:text-brand" href="/">home</a>
+					<a className="after:transition-all text-gery-text uppercase p-2 tracking-wider text-sm transition-all relative mr-2 last-of-type:mr-0 hover:text-brand" href="/">how we work</a>
+					<a className="after:transition-all text-gery-text uppercase p-2 tracking-wider text-sm transition-all relative mr-2 last-of-type:mr-0 hover:text-brand" href="/">team</a>
+					<a className="after:transition-all text-gery-text uppercase p-2 tracking-wider text-sm transition-all relative mr-2 last-of-type:mr-0 hover:text-brand" href="/">we&apos;re hiring!</a>
 					<button
 						className="flex items-center uppercase"
 						type="button"
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"gery-text": "#d1d5db",
+			},
+		},
 	},
 	plugins: [],
 };
