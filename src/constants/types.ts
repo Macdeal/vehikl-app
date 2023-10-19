68169a8ec3a5c1a8589d7d1cc795b8f19f2d7870
Akhil P Jose <akhil.jose@vonnue.com>
2023-10-19 23:58:13 +0530

    feat: text container

---
+++ src/constants/types.ts
@@ -0,0 +1,6 @@
+enum TextAlign {
+	CENTER = 'center-align',
+	LEFT = 'left-align',
+}
+
+export default TextAlign;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,10 +4,14 @@ import EventCardCollection from '../container/EventCardCollection';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
+import TextContainer from '../components/TextContainer';
+import TextAlign from '../constants/types';
+
 const HomePage = () => (
   <div>
     <Header />
     <EventCardCollection cardData={tempEventCardCollectionData} />
+    <TextContainer title="Hello World" description="#include stdio.h" variation={TextAlign.CENTER} />
     <Footer />
   </div>
 );
