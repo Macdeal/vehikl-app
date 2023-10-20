f0e5bda82f351958ec838dea13c82c2307cc3118
Akhil P Jose <akhil.jose@vonnue.com>
2023-10-20 14:23:46 +0530

    refactor: sort import

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,4 @@
-import { EventVariation } from '../../data';
 import Facebook from '../../assets/footer/facebook.svg?react'
-import FooterData from '../../dummydata/Footer';
 import Instagram from '../../assets/footer/instagram.svg?react'
 import LinkedIn from '../../assets/footer/linkedIn.svg?react'
 import Logo from '../../assets/header/logo.svg?react';
@@ -8,6 +6,8 @@ import Podcast from '../../assets/footer/podcast.svg?react'
 import Twitter from '../../assets/footer/twitter.svg?react'
 import YouTube from '../../assets/footer/youtube.svg?react'
 import EventTitle from '../eventTitle/EventTitle';
+import FooterData from '../../dummydata/Footer';
+import { EventVariation } from '../../data';
 
 const Footer = () => (
 	<>
